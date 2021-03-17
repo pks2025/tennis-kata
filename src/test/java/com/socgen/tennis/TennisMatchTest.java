d75044a72bbe97ec2ed0a5e1f49b61302fd65cc1
Pramod_Sinha <pramod_sinha@Dellteam.com>
2021-03-17 09:37:30 +0530

    initial TennisMatch  test class

---
+++ src/test/java/com/socgen/tennis/TennisMatchTest.java
@@ -0,0 +1,39 @@
+package com.socgen.tennis;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+public class TennisMatchTest {
+
+    @Test
+    void createTennisMatch() {
+        TennisMatch tennisMatch = new TennisMatch("Tennis Match");
+        assertNotNull(tennisMatch, "should able to create a Tennis match");
+    }
+
+    @Test
+    void shouldAddRefereeToTennisMatch(){
+        TennisMatch tennisMatch = new TennisMatch("Tennis Match");
+
+        Referee referee = new Referee("Bharat");
+        tennisMatch.setReferee(referee);
+
+        assertNotNull(tennisMatch.getReferee());
+        assertEquals("Bharat", tennisMatch.getReferee().getName(), "referee should have name");
+    }
+
+    @Test
+    void shouldSetPlayersToTennisMatch() {
+        TennisMatch tennisMatch = new TennisMatch("Tennis Match");
+
+        Referee referee = new Referee("Bharat");
+        referee.setFirstPlayer(new Player("Roger"));
+        referee.setSecondPlayer(new Player("Nick"));
+
+        tennisMatch.setReferee(referee);
+
+        assertNotNull(tennisMatch.getReferee().getFirstPlayer(), "should able to set first player");
+        assertNotNull(tennisMatch.getReferee().getSecondPlayer(), "should able to set second player");
+    }
+}
