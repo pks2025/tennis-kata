a7b212948c15f3733e7ef5b32bff1284732049ba
Pramod_Sinha <pramod_sinha@Dellteam.com>
2021-03-17 09:38:41 +0530

    initial TennisMatchApp test class

---
+++ src/test/java/com/socgen/tennis/TennisMatchAppTest.java
@@ -0,0 +1,28 @@
+package com.socgen.tennis;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Scanner;
+
+public class TennisMatchAppTest {
+
+    @Test
+    void shouldCreateTennisMatch() {
+        Scanner scanner = new Scanner(System.in);
+
+        TennisMatch tennisMatch =  TennisMatchApp.initTennisMatch(scanner);
+
+        assertNotNull(tennisMatch, "should able to create tennis match");
+    }
+
+    @Test
+    void shouldStartTennisMatch() {
+        Scanner scanner = new Scanner(System.in);
+
+        TennisMatch tennisMatch =  TennisMatchApp.initTennisMatch(scanner);
+
+        TennisMatchApp.startTennis(tennisMatch, scanner);
+
+        assertNotNull(tennisMatch.getReferee().getWinner(), "should get winner at end of tennis set match");
+    }
+}
